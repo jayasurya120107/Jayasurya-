8f6678acbc68da1efcdcea26ba6628dc72f350bf
jayasurya120107 <jaya120107@gmail.com>
2025-09-13 06:11:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Handle form submission
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+
+  e.preventDefault();
+
+  alert("Thanks for your message! I will get back to you soon.");
+
+  this.reset();
+
+});
